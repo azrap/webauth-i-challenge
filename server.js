e286afb3874ab758b4b4cf020d32782944e43ded
azrap <azraap@gmail.com>
2019-06-10 14:25:10 -0700

    acheived MVP

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
-//instantiating server
 const server = express();
-
-//global middleware:
 server.use(express.json()); // built-in mw
 // server.use(logger);
+const bcrypt=require('bcrypt');
+const Users=require('./data/user-model.js');
+const restricted = require('./data/restricted.js')
+
 
 
 server.get('/', (req, res) => {
@@ -13,47 +13,65 @@ server.get('/', (req, res) => {
   });
 
 
-//   router.post('/projects', async (req, res) => {
-//     try {
-//         const project= await Projects.addProject(req.body)
-//         res.status(201).json(project);
-//     }
-//     catch (error){
-//         console.log(error);
-//         res.status(500).json({
-//           message: 'Error creating a new project suckas',
-//         });
+  server.post('/api/register', async (req, res) => {
+    try {
+        userInfo=req.body
+        console.log(userInfo.username)
+        userInfo.password = await bcrypt.hashSync(userInfo.password, 10)
+        const user = await Users.addUser(userInfo)
+        res.status(201).json(user);
+    }
+
+    catch (error){
+        console.log(error);
+        res.status(500).json({
+          message: 'Error creating a new user suckas',
+        });
   
-//     }
-//   });
+    }
+  });
 
-//   router.post('/actions', async (req, res) => {
-//     try {
-//         const action= await Projects.addAction(req.body)
-//         res.status(201).json(action);
-//     }
-//     catch (error){
-//         console.log(error);
-//         res.status(500).json({
-//           message: 'Error creating a new action suckas',
-//         });
+  server.post('/api/login', async (req, res) => {
 
-//     }
-// });
+    let {username, password} = req.body;
+    console.log(username)
+    console.log(password)
 
-// router.get('/projects', async (req, res) => {
-//     try {
+    try {
+        const user = await Users.findBy( { username })
+        console.log(user);
+        console.log();
+
+        if (user && bcrypt.compareSync(password, user[0].password)){
+            res.status(200).json({message:`Welcome user!`});
+
+        }
+        else {
+            res.status(401).json({message: 'Invalid Credentials'})
+        }
+    }
+    catch (error){
+        console.log(error);
+        res.status(500).json({
+          message: 'Error creating a new login suckas',
+        });
+
+    }
+});
+
+server.get('/api/users', restricted, async (req, res) => {
+    try {
       
-//       const projects = await Projects.findProjects();
-     
-//       res.status(200).json(projects);
-//     } catch (error) {
+     const users= await Users.findUsers()
+    
+      res.status(200).json(users);
+    } catch (error) {
       
-//       res.status(500).json({
-//         message: 'Error retrieving the dang projects',
-//       });
-//     }
-//   });
+      res.status(500).json({
+        message: 'Error retrieving the dang users',
+      });
+    }
+  });
 
 
 //   //routing and API:
